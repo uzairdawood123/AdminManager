ef82f975aa22f99720c6bb86d2831876b1fcdd0c
Uzair123 <uzairdawood05@gmail.com>
2020-05-05 03:31:29 +0500

    second push this is not that much changes

---
+++ app/src/main/java/com/example/adminmanagermvvm/feature/Expenses/ui/AddExpenseActivity.kt
@@ -0,0 +1,13 @@
+package com.example.adminmanagermvvm.feature.Expenses.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.adminmanagermvvm.R
+
+class AddExpenseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_expense)
+    }
+}
---
+++ app/src/main/java/com/example/adminmanagermvvm/feature/ToDo/ui/AddTodo.kt
@@ -0,0 +1,13 @@
+package com.example.adminmanagermvvm.feature.ToDo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.adminmanagermvvm.R
+
+class AddTodo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_todo)
+    }
+}
